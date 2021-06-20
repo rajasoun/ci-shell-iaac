7a4cafcf96a3ad1fbb7f69d7321e26a0ae83e8bc
Raja Soundaramourty <rajasoun@icloud.com>
2021-06-20 23:45:13 +0530

    Build and Publish Docker Container

--- ci-shell/src/lib/docker.sh
+++ ci-shell/src/lib/docker.sh
@@ -12,7 +12,7 @@ function load_env_variables(){
     _get_docker_file_path "$CONFIG_JSON" 
     debug "DOCKER_FILE_PATH: $DOCKER_FILE_PATH"
 
-    BUILD_CONTEXT="."
+    BUILD_CONTEXT="vscode-iaac/${SHELL}/.devcontainer"
     BUILD_ARGS=$(_get_build_args "$CONFIG_JSON")
     debug "BUILD_ARGS : $BUILD_ARGS"
     DOCKER_IMAGE="vsc-$(_get_app_name_from_git_workspace):$(git rev-parse HEAD)"
